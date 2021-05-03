27b7b878342f8d4105a8573823990d76946929fe
JEONGMI PARK <jeongmipark@JEONGMIui-MacBookPro.local>
2021-05-03 15:27:55 +0900

    ts block sample

---
+++ src/ts_study03_block.ts
@@ -0,0 +1,27 @@
+class Block{
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data:string;
+    public timestamp:number;
+
+    constructor(
+        index:number,
+        hash:string,
+        previousHash:string,
+        data:string,
+        timestamp:number
+        ){
+            this.index=index;
+            this.hash=hash;
+            this.previousHash=previousHash;
+            this.data=data;
+            this.timestamp=timestamp;
+        }
+}
+
+const genesisBlock:Block=new Block(0,"2920203229","","Hello",123456);
+let blockchain:[Block]=[genesisBlock];
+// blockchain.push("stuuf");
+console.log(blockchain);
+export {};
